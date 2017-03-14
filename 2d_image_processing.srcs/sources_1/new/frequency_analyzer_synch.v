3779221363af576f716b54bc657d425cceeca043
EvilLord666 <um.nix.user@gmail.com>
2017-03-15 01:04:42 +0400

    Подключение портов на блок дизайне

---
+++ 2d_image_processing.srcs/sources_1/new/frequency_analyzer_synch.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.03.2017 23:31:58
+// Design Name: 
+// Module Name: frequency_analyzer_synch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module frequency_analyzer_synch(
+    input clock,
+    input reset,
+    input enable,
+    output start_analyzer_0,
+    output stop_analyzer_0,
+    output start_analyzer_1,
+    output stop_analyzer_1
+    );
+endmodule
