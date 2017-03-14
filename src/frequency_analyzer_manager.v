6c9005dd880d2623c895c010874e11193f4c2a42
Unknown <um.nix.user@gmail.com>
2017-03-14 19:28:42 +0500

    Переименование проекта

---
+++ src/frequency_analyzer_manager.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/13/2017 05:12:24 PM
+// Design Name: 
+// Module Name: frequency_analyzer_manager
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module frequency_analyzer_manager(
+    input [7:0] data,
+    input clk,
+    input enable
+    );
+endmodule
