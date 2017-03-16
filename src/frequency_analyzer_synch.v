3c4f8993ce8fb9bfd348ca395e55dc3dd12f7802
Unknown <um.nix.user@gmail.com>
2017-03-16 14:45:22 +0500

    Тест на синхронизатор, синхронизатор работает, но пришлось воткнуть здоровенный if-else

--- src/frequency_analyzer_synch.v
+++ src/frequency_analyzer_synch.v
@@ -29,36 +29,93 @@ module frequency_analyzer_synch #
     input wire clock,
     input wire reset,
     input wire enable,
-    output wire start_analyzer_0,
-    output wire stop_analyzer_0,
-    output wire start_analyzer_1,
-    output wire stop_analyzer_1
+    output reg start_analyzer_0,
+    output reg stop_analyzer_0,
+    output reg start_analyzer_1,
+    output reg stop_analyzer_1
 );
 
     localparam integer frequency_ticks = CLOCK / FREQUENCY;
-    localparam integer delay = 20;
-    integer count;
-    
-    wire start_0_done;
-    wire stop_0_done;
-    wire start_1_done;
-    wire stop_1_done;
-    
-    COUNTER_TC_MACRO #(.COUNT_BY(delay), .DEVICE("7SERIES"), .DIRECTION("UP"), .RESET_UPON_TC("FALSE"), .WIDTH_DATA(32))
-    start_0_counter(.TC(start_0_done), .CLK(clock), .CE(enable), .RST(reset | stop_1_done));
-     
-    COUNTER_TC_MACRO #(.COUNT_BY(frequency_ticks), .DEVICE("7SERIES"), .DIRECTION("UP"), .RESET_UPON_TC("FALSE"), .WIDTH_DATA(32))
-    stop_0_counter(.TC(stop_0_done), .CLK(clock), .CE(enable & start_0_done), .RST(reset | stop_1_done));
+    localparam integer signal_delay = 20;
+    integer clock_counter;
 
-    COUNTER_TC_MACRO #(.COUNT_BY(frequency_ticks), .DEVICE("7SERIES"), .DIRECTION("UP"), .RESET_UPON_TC("FALSE"), .WIDTH_DATA(32))
-    start_1_counter(.TC(start_1_done), .CLK(clock), .CE(enable & stop_0_done), .RST(reset | stop_1_done));
-    
-    COUNTER_TC_MACRO #(.COUNT_BY(delay), .DEVICE("7SERIES"), .DIRECTION("UP"), .RESET_UPON_TC("FALSE"), .WIDTH_DATA(32))
-    stop_1_counter(.TC(stop_1_done), .CLK(clock), .CE(enable & start_1_done), .RST(reset));
-    
-    assign start_analyzer_0 = enable ? ~start_0_done : 0;
-    assign stop_analyzer_0 = enable ? stop_0_done & ~start_1_done : 0;
-    assign start_analyzer_1 = enable ? ~start_1_done : 0;
-    assign stop_analyzer_1 = enable ? ~stop_1_done & ~start_0_done : 0;
+	
+	always @(posedge clock)
+	begin
+	    if(reset)
+		    clock_counter <= 0;
+	    else
+		begin
+		    if(enable)
+			begin
+			    clock_counter <= clock_counter + 1;
+				if(clock_counter >= frequency_ticks + frequency_ticks + signal_delay)
+				    clock_counter <= 0;
+			end
+		end
+	end
+	
+	always @(posedge clock)
+	begin
+	    if(reset)
+		begin
+		    start_analyzer_0 <= 0;
+			stop_analyzer_0 <= 0;
+			start_analyzer_1 <= 0;
+			stop_analyzer_1 <= 0;
+		end
+		else
+		begin
+		    if(enable)
+			begin
+			    if(clock_counter < signal_delay )
+				begin
+				    start_analyzer_0 <= 1;
+					stop_analyzer_0 <= 0;
+					start_analyzer_1 <= 0;
+					stop_analyzer_1 <= 0;
+				end
+				else
+				begin
+				    if(clock_counter >= signal_delay && clock_counter < frequency_ticks)
+					begin
+					    start_analyzer_0 <= 0;
+					    stop_analyzer_0 <= 0;
+					    start_analyzer_1 <= 0;
+					    stop_analyzer_1 <= 0;
+					end
+					else
+					begin
+					    if(clock_counter >= frequency_ticks && clock_counter < frequency_ticks + signal_delay)
+						begin
+						    start_analyzer_0 <= 0;
+						    stop_analyzer_0 <= 1;
+					        start_analyzer_1 <= 1;
+							stop_analyzer_1 <= 0;
+						end
+						else
+						begin
+						    if(clock_counter >= frequency_ticks + signal_delay &&
+							   clock_counter < frequency_ticks + frequency_ticks)
+							begin
+						        start_analyzer_0 <= 0;
+					            stop_analyzer_0 <= 0;
+					            start_analyzer_1 <= 0;
+					            stop_analyzer_1 <= 0;
+							end
+							else
+							begin
+								start_analyzer_0 <= 1;
+					            stop_analyzer_0 <= 0;
+					            start_analyzer_1 <= 0;
+					            stop_analyzer_1 <= 1;
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+	
 
 endmodule
