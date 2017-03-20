e360fb5f43b254cb7b1e0a8f99f00ccd44d18103
EvilLord666 <um.nix.user@gmail.com>
2017-03-20 21:19:48 +0400

    Модуль для тестирования frequency_analyzer_manager, осталось написать скрипт

---
+++ tests/frequency_analyzer_manager_tests/frequency_analyzer_manager_testbench.v
@@ -0,0 +1,75 @@
+module frequency_analyzer_manager_testbench #
+(
+    // Parameters of Axi Slave Bus Interface S00_AXI
+    parameter integer C_S00_AXI_DATA_WIDTH = 32,
+    parameter integer C_S00_AXI_ADDR_WIDTH = 10
+)
+(
+    input wire [7:0] data,
+    input wire pixel_clock,
+    input wire start,
+    input wire stop,
+    input wire clear,
+    output wire irq,
+    // Ports of Axi Slave Bus Interface S00_AXI
+    input wire  s00_axi_aclk,
+    input wire  s00_axi_aresetn,
+    input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+    input wire [2 : 0] s00_axi_awprot,
+    input wire  s00_axi_awvalid,
+    output wire  s00_axi_awready,
+    input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+    input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+    input wire  s00_axi_wvalid,
+    output wire  s00_axi_wready,
+    output wire [1 : 0] s00_axi_bresp,
+    output wire  s00_axi_bvalid,
+    input wire  s00_axi_bready,
+    input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+    input wire [2 : 0] s00_axi_arprot,
+    input wire  s00_axi_arvalid,
+    output wire  s00_axi_arready,
+    output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+    output wire [1 : 0] s00_axi_rresp,
+    output wire  s00_axi_rvalid,
+    input wire  s00_axi_rready
+);
+
+    frequency_analyzer_manager #
+	(
+	    .C_S00_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+		.C_S00_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+	) 
+	testing_frequency_analyzer_manager
+	(
+	    .data(data),
+        .pixel_clock(pixel_clock),
+        .start(start),
+        .stop(stop),
+        .clear(clear),
+        .irq(irq),
+    // Ports of Axi Slave Bus Interface S00_AXI
+        .s00_axi_aclk(s00_axi_aclk),
+        .s00_axi_aresetn(s00_axi_aresetn),
+        .s00_axi_awaddr(s00_axi_awaddr),
+        .s00_axi_awprot(s00_axi_awprot),
+        .s00_axi_awvalid(s00_axi_awvalid),
+        .s00_axi_awready(s00_axi_awready),
+        .s00_axi_wdata(s00_axi_wdata),
+        .s00_axi_wstrb(s00_axi_wstrb),
+        .s00_axi_wvalid(s00_axi_wvalid),
+        .s00_axi_wready(s00_axi_wready),
+        .s00_axi_bresp(s00_axi_bresp),
+        .s00_axi_bvalid(s00_axi_bvalid),
+        .s00_axi_bready(s00_axi_bready),
+        .s00_axi_araddr(s00_axi_araddr),
+        .s00_axi_arprot(s00_axi_arprot),
+        .s00_axi_arvalid(s00_axi_arvalid),
+        .s00_axi_arready(s00_axi_arready),
+        .s00_axi_rdata(s00_axi_rdata),
+        .s00_axi_rresp(s00_axi_rresp),
+        .s00_axi_rvalid(s00_axi_rvalid),
+        .s00_axi_rready(s00_axi_rready)
+	);
+
+endmodule
