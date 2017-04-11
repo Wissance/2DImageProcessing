aabed0c8a1eef10718df035a6a03c5ce23ef2dd9
EvilLord666 <um.nix.user@gmail.com>
2017-04-11 08:31:50 +0400

    Более удобная работа скриптов

--- tests/frequency_analyzer_manager_tests/frequency_analyzer_manager.tcl
+++ tests/frequency_analyzer_manager_tests/frequency_analyzer_manager.tcl
@@ -1,3 +1,4 @@
+restart
 add_force {/frequency_analyzer_manager_testbench/data} -radix hex {0 0ns} {0xf0 0.7us} {0x40 0.8us} {0xf0 21.18us} {0x40 21.48us} {0xf0 41.66us} {0x40 41.96us} {0xf0 62.14us} {0x40 62.44us} {0xf0 82.62us} {0x40 82.92us} {0xf0 103.1us} {0x40 103.4us} {0xf0 225.98us} {0x00 226.28us}
 #{0 0ns} {0xff 0.7us} {0x00 1us} {0xff 10.38us} {0x00 10.42us} {0xff 20.50us} {0x00 21.0us} {0xff 21.1us} {0x00 21.5us} {0xff 41.1us} {0x00 41.8us} {0xff 61.6us} {0x00 62.4us} {0xff 123us}
 add_force {/frequency_analyzer_manager_testbench/pixel_clock} -radix hex {1 0ns} {0 10ns} -repeat_every 20ns
@@ -6,3 +7,4 @@ add_force {/frequency_analyzer_manager_testbench/stop} -radix hex {0 0ns} {1 499
 add_force {/frequency_analyzer_manager_testbench/clear} -radix hex {0} {1 100ns} {0 5000ms} 
 add_force {/frequency_analyzer_manager_testbench/s00_axi_aresetn} -radix hex {0} {1 100ns} {0 5000ms} 
 add_force {/frequency_analyzer_manager_testbench/s00_axi_aclk} -radix hex {1 0ns} {0 5ns} -repeat_every 10ns
+run 1000 ms
--- tests/image_capture_manager_tests/image_capture_manager_test.tcl
+++ tests/image_capture_manager_tests/image_capture_manager_test.tcl
@@ -1,3 +1,4 @@
+restart
 #AXI Signals
 add_force {/image_capture_manager_testbench/aclk} -radix hex {1 0ns} {0 5000ps} -repeat_every 10000ps
 add_force {/image_capture_manager_testbench/aresetn} -radix hex {0} {1 100us} {0 970us} 
@@ -11,4 +12,4 @@ add_force {/image_capture_manager_testbench/wstrb} -radix hex {0} {0x1 250us} {0
 add_force {/image_capture_manager_testbench/wvalid} -radix hex {0} {1 250us} {0 450us} {1 600us} {0 800us}
 add_force {/image_capture_manager_testbench/bready} -radix hex {0} {1 450us} {0 550us} {1 800us} {0 900us}
 
-
+run 1ms
