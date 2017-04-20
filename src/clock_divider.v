e8b595d2b96144ddb74201fad5a061932535247e
Unknown <um.nix.user@gmail.com>
2017-04-20 11:41:04 +0500

    Фикс делителя  тактовой частоты

--- src/clock_divider.v
+++ src/clock_divider.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: EasySoft Group
+// Engineer: Ushakov M.V. (EvilLord666)
 // 
 // Create Date: 04.10.2016 05:53:55
 // Design Name: 
 // Module Name: clock_divider
 // Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Target Devices: Zynq-7k
+// Tool Versions: Vivado 2016.2
+// Description: Module for programmable clock division from 1 till 255
 // 
 // Dependencies: 
 // 
-// Revision:
-// Revision 0.01 - File Created
+// Revision: 
+// Revision 1.0
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -25,42 +25,33 @@ module clock_divider #
     parameter clock_division = 2
 )
 (
+    input wire reset,
     input wire input_clock,
-    output reg output_clock = 0
+    output wire output_clock
 ); 
-    //reg[7:0] counter;
+    reg[7:0] counter;
+    reg output_clock_value;
     
-    //initial counter = 8'b00000000;
-    
-    integer counter = 0;
+    assign output_clock =  clock_division == 1 ? input_clock & reset : output_clock_value; 
 
     
-/*    if (clock_division == 1)
-    begin            
-        always@ (input_clock)
-        begin
-            output_clock <= input_clock;
-        end
-    end
-    
-    else
+    always @(posedge input_clock)
     begin
-        always@ (posedge input_clock)
-        begin
-            counter = counter + 1;
-            if(counter == clock_division)
-            begin
-                output_clock = ~output_clock;
-                counter = 0;
-            end
-        end
-    end*/
-    
-    always@ (input_clock)
-    begin
-        if(clock_division == 1)
-        begin
-        end
+         if(~reset)
+         begin
+             counter <= 0;
+             output_clock_value <= 0;
+         end
+         else
+         begin
+             if(counter == clock_division - 1)
+             begin
+                 output_clock_value <= ~output_clock_value;
+                 counter <= 1;
+             end
+             else
+                 counter <= counter + 1;
+         end
     end
     
 endmodule
