6cfb900b1af51b7a426e3c55e60fe5a7706371f3
VeryNiceGuy <killingmesoftlywithchainsaw@gmail.com>
2017-05-29 06:07:32 +0500

    Добавлена константа для 80МГц

--- src/linescanner_image_capture_unit.v
+++ src/linescanner_image_capture_unit.v
@@ -30,6 +30,7 @@ module linescanner_image_capture_unit(
     localparam clocks_per_microsecond_50_mhz = 48;
     localparam clocks_per_microsecond_60_mhz = 58;
     localparam clocks_per_microsecond_70_mhz = 68;
+    localparam clocks_per_microsecond_80_mhz = 78;
     localparam clocks_per_microsecond = clocks_per_microsecond_50_mhz;
       
     reg[7:0] sm1_state;
