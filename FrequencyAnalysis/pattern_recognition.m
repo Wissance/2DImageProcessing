cbd7d4b6192a68d8c639ee487a5a2f8ca8ec4fb4
VeryNiceGuy <killingmesoftlywithchainsaw@gmail.com>
2017-09-12 22:19:19 +0500

    Набросок симуляции детектора прыжка частоты
    Складывается впечатление  что дискретизации в 50kHz недостаточно для детектирования по паттерну.

---
+++ FrequencyAnalysis/pattern_recognition.m
@@ -0,0 +1,56 @@
+%{ Выставляем частоту дискретизации. %}
+sampling_frequency = 50000;
+
+%{ Создаем временной вектор продолжительностью в 0.5 секунды. %}
+time = (0:1/sampling_frequency:0.5)';
+
+%{ Задаем интересующие нас частоты. %}
+frequency_1 = 21000;
+frequency_2 = 18000;
+
+%{ Синтезируем две квадратные волны. %}
+square_wave_1 = (square(frequency_1 * 2 * pi * time, 50) + 1) * 0.5;
+square_wave_2 = (square(frequency_2 * 2 * pi * time, 50) + 1) * 0.5;
+
+%{ Конкатенируем половины обеих волн для симуляции прыжка частоты. %}
+square_wave_3 = [...
+    square_wave_1(1:sampling_frequency/4);...
+    square_wave_2(sampling_frequency/4+1:sampling_frequency/2+1)];
+
+%{ Создаем скоп для каждой волны. %}
+scope_1 = dsp.TimeScope(...
+    'Name', '21kHz square wave',...
+    'SampleRate',sampling_frequency,...
+    'TimeSpan',0.5);
+
+scope_2 = dsp.TimeScope(...
+    'Name', '18kHz square wave',...
+    'SampleRate',sampling_frequency,...
+    'TimeSpan',0.5);
+
+scope_3 = dsp.TimeScope(...
+    'Name', '21+18kHz square wave',...
+    'SampleRate',sampling_frequency,...
+    'TimeSpan',0.5);
+
+%{ Передаем волны в скопы. %}
+scope_1(square_wave_1);
+scope_2(square_wave_2);
+scope_3(square_wave_3);
+
+%{
+    Итерируем через массив семплов обеих волн отбирая numberOfSamples
+    для анализа паттерна.
+%}
+
+i = 1;
+numberOfSamples = 6;
+
+while i + numberOfSamples <= sampling_frequency / 2 + 1
+    pattern = square_wave_3(i:i+numberOfSamples-1)
+    
+    i = i + numberOfSamples;
+end
+
+    
+
